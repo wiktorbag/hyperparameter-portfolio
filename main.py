7e03e1154487f056dd3811bcf911c09a8b69ea6c
wiktorbag <116521897+wiktorbag@users.noreply.github.com>
2024-11-16 22:00:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
+from functions import *
+import pandas as pd
 
 def main():
-    pass
-
+    file = pd.read_csv("MementoML.csv")
+    models = ['gbm', 'glmnet', 'kknn', 'randomForest']
+    portofolios = []
+    for model in models:
+        model_auc = get_mean_metric_dataset(file=file,metric="auc", model=model )
+        portofolios.append(ASMFO(file=model_auc, T=100))
+    
+    df = pd.DataFrame({name: col for name, col in zip(models, portofolios)})
+    output_path = r"\github\hyperparameter-portfolio\portfolios.csv"
+    df.to_csv(output_path, index=False)
+    
 if __name__ == '__main__':
     main()
 
