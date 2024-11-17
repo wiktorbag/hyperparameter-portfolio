1a8bdd2315be20ea1f721d4ce6423dd334b66712
wiktorbag <116521897+wiktorbag@users.noreply.github.com>
2024-11-18 00:31:30 +0100

    Update main.py
    Deleting the param_indexes 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, whish are not present in MementoML documentation

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import pandas as pd
 
 def main():
     file = pd.read_csv("MementoML.csv")
+    # Deleting the param_indexes 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, whish are not present in MementoML documentation
+    file = file[~file["param_index"].isin([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])]
+    
     models = ['gbm', 'glmnet', 'kknn', 'randomForest']
     portofolios = []
     for model in models:
